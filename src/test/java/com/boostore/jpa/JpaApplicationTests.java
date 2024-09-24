0dfb184ca105156147909616d810c9494a85e601
Victor Emanuel <victoremanuel12@hotmail.com>
2024-09-23 23:42:02 -0300

    first commit

---
+++ src/test/java/com/boostore/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boostore.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
